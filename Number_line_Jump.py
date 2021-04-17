17dbbb38961e8dc629d374042e32da21dca6c7fa
Saikumar-ak <81463755+saikumar-aks@users.noreply.github.com>
2021-04-17 09:59:33 +0530

    Create Number_line_Jump.py

---
+++ Number_line_Jump.py
@@ -0,0 +1,13 @@
+# <problem link :- https://www.hackerrank.com/challenges/kangaroo/problem >
+
+x1, v1, x2, v2 = map(int, input().split(' '))
+if x1-v1==0:
+    print("YES")
+    quit()
+if v2-v1==0:
+    print("NO")
+    quit()
+if ((x1-x2)%(v2-v1)==0 and (x1-x2)//(v2-v1)>=0):
+    print("YES")
+    quit()
+print("NO")
