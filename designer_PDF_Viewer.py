6ffc16e11340fb91d35d5918549802af2db77fff
Saikumar-ak <81463755+saikumar-aks@users.noreply.github.com>
2021-05-02 13:41:08 +0530

    Create designer_PDF_Viewer.py

---
+++ designer_PDF_Viewer.py
@@ -0,0 +1,6 @@
+# problem :- https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+def designerPdfViewer(h, word):
+    return max([h[ord(i)-97] for i in word])*len(word)
+h = list(map(int,input().split()))
+word = input()
+print(designerPdfViewer(h, word))
