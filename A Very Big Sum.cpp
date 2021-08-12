a26f1bcb5d990a4b412f2db305be3cb4b4944315
Saikumar-ak <81463755+saikumar-aks@users.noreply.github.com>
2021-08-12 23:14:56 +0530

    Create A Very Big Sum.cpp

---
+++ A Very Big Sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using std::cin;
+using std::cout;
+using std::endl;
+#define Log(Msg) cout<<Msg<<endl
+int main()
+{
+	int num;
+	cin>>num;
+	long long int val, sum=0;
+	for(int i=0;i<num;++i)
+	{
+		cin>>val;
+		sum+=val;
+	}
+
+}
